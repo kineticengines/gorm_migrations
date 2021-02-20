6a352ac896cc2d060b0a43cd25b6c15d7b63e52b
David Dexter <dmwangi@kineticengines.co.ke>
2021-02-20 21:49:59 +0300

    feat : initialize command (#1)
    Signed-off-by: DavidDexter <dmwangi@kineticengines.co.ke>

--- commands/intialize.go
+++ commands/intialize.go
@@ -1,6 +1,13 @@
 package commands
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"text/template"
+
 	"github.com/kineticengines/gorm-migrations/models"
 	"github.com/urfave/cli/v2"
 )
@@ -10,6 +17,74 @@ var IntializeCmd = &cli.Command{
 	Name:  models.IntializeCmd,
 	Usage: models.IntializeCmdUsage,
 	Action: func(c *cli.Context) error {
-		return nil
+		return initialize()
 	},
+}
+
+var initTemplate = template.Must(template.New("name").Parse(
+	`# Where all migrations files will be located
+migrations:
+  - migrations/*.gormgx
+
+# Where your gorm models are located
+models:
+  - models/models.go  
+
+# Optional: set to add "gorm.Model" to your models
+add_gorm_model: true
+
+`))
+
+func initialize() error {
+	exists, err := checkIfInitialized()
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return createGormgxYamlFile()
+	}
+	return models.ErrGormgxYamlExists
+}
+
+func gormgxFilePath() (*string, error) {
+	path, err := os.Getwd()
+	if err != nil {
+		return nil, models.ErrUnableToGetWorkingDirectory
+	}
+	file := filepath.Join(path, "gormgx.yaml")
+	return &file, nil
+}
+
+// checkIfInitialized checks for the presence of gormgx.yaml file
+// returns an error if it absent
+func checkIfInitialized() (bool, error) {
+	file, err := gormgxFilePath()
+	if err != nil {
+		return false, err
+	}
+	if _, err := os.Stat(*file); os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, nil
+}
+
+func createGormgxYamlFile() error {
+	path, err := gormgxFilePath()
+	if err != nil {
+		return err
+	}
+	_, err = os.OpenFile(*path, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+
+	var buf bytes.Buffer
+	if err := initTemplate.Execute(&buf, ""); err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(*path, buf.Bytes(), 0644); err != nil {
+		return fmt.Errorf("unable to write gormgx file: " + err.Error())
+	}
+	return nil
 }
